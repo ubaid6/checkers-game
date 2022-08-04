8da1d0d713783fc5c96599f02b69eac7a0c68345
Ubaid <ubaid.khan2@yahoo.com>
2022-08-04 11:36:19 +0100

    Board component moed to a different file

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,42 +1,6 @@
 import React from 'react';
 import './Game.css';
-import {Square} from './Square';
-
-
-
-
-class Board extends React.Component<any, any> {
-
-  renderSquare(piece: string, index: Array<any>) {
-    return <Square piece={piece}
-      onClick={() => this.props.onClick(index)} />;
-  }
-
-  makeBoard(boardState: Array<Array<string>>) {
-    var boardRows: any[] = new Array(8);
-    for (let i = 0; i < 8; i++) {
-      var row: any[] = new Array(8);
-      for (let j = 0; j < 8; j++) {
-        row.push(this.renderSquare(boardState[i][j], [i, j]));
-      }
-      boardRows.push(
-        <div>
-          {row}
-        </div>
-      )
-    }
-    return boardRows;
-  }
-
-  render() {
-    return (
-      <div className="board">
-        {this.makeBoard(this.props.boardState)}
-      </div>
-    );
-  }
-}
-
+import {Board} from './Board';
 
 
 class Game extends React.Component<any, any> {
