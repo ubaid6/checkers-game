0fa3b3efb0c01eaed8b684ebcab97052175ec01d
Ubaid <ubaid.khan2@yahoo.com>
2022-08-04 12:33:02 +0100

    Board component changed to functional component

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,36 +1,34 @@
 import React from 'react'
-import {Square} from './Square';
+import { Square } from './Square';
 
 
-export class Board extends React.Component<any, any> {
+export const Board = (props: any): JSX.Element => {
 
-    renderSquare(piece: string, index: Array<any>) {
-      return <Square piece={piece}
-        onClick={() => this.props.onClick(index)} />;
+    const renderSquare = (piece: string, index: Array<any>): JSX.Element => {
+        return <Square piece={piece}
+            onClick={() => props.onClick(index)} />;
     }
-  
-    makeBoard(boardState: Array<Array<string>>) {
-      var boardRows: any[] = new Array(8);
-      for (let i = 0; i < 8; i++) {
-        var row: any[] = new Array(8);
-        for (let j = 0; j < 8; j++) {
-          row.push(this.renderSquare(boardState[i][j], [i, j]));
+
+    const makeBoard = (boardState: Array<Array<string>>): Array<Array<JSX.Element>> => {
+        var boardRows: any[] = new Array(8);
+        for (let i = 0; i < 8; i++) {
+            var row: any[] = new Array(8);
+            for (let j = 0; j < 8; j++) {
+                row.push(renderSquare(boardState[i][j], [i, j]));
+            }
+            boardRows.push(
+                <div>
+                    {row}
+                </div>
+            )
         }
-        boardRows.push(
-          <div>
-            {row}
-          </div>
-        )
-      }
-      return boardRows;
+        return boardRows;
     }
-  
-    render() {
-      return (
+
+
+    return (
         <div className="board">
-          {this.makeBoard(this.props.boardState)}
+            {makeBoard(props.boardState)}
         </div>
-      );
-    }
-  }
-  
+    );
+}
